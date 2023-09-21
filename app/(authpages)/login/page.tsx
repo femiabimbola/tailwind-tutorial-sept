a18e1b7dac180eec23bd82b68555e9190a27b366
femi <femiabimbola6@gmail.com>
2023-09-21 20:38:17 +0100

    worked on register-page

--- app/(authpages)/login/page.tsx
+++ app/(authpages)/login/page.tsx
@@ -1,53 +1,44 @@
 const LoginPage = () => {
   return (
     <div className="flex items-center h-screen">
-      <div className="hidden bg-slate-300  h-screen md:flex md:w-2/3 items-center">
-        Login to your account
+
+      <div className="hidden bg-slate-300 h-screen md:flex md:w-2/3  items-center">
+        <h2 className="m-auto text-2xl px-11 text-center font-extrabold"> Embark on an educational journey where learning meets thrilling adventure</h2>
       </div>
-      <div className="flex bg-white-500  items-center justify-center">
-        <div className=" bg-white rounded-lg ml-16 justify-center shadow-lg p-12">
-          <h2 className="text-2xl text-center font-bold mb-4">Login</h2>
-          <h3 className="text-center"> If you don't have an account <a className='text-blue-700' href="./register/"> create one </a></h3>
-          <form>
-            <div>
-              <label htmlFor="username" className="block text-gray-700 font-bold mb-2">
-                Username
-              </label>
-              <input type="email" id="email"
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-                placeholder="Enter your email"
-              />
-            </div>
-            <div>
-              <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
-                Email
-              </label>
-              <input type="email" id="email"
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-                placeholder="Enter your email"
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="password" className="block text-gray-700 font-bold mb-2">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-                placeholder="Enter your password"
-              />
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-indigo-500 text-white font-bold py-2 px-4 rounded-md hover:bg-indigo-600"
-            >
-              Login
-            </button>
-          </form>
-          <p className="text-center text-blue-700 mt-3"><a>Forget your password?</a></p>
-        </div>
+      <div className=" bg-white rounded-lg m-auto shadow-lg p-12">
+        <h2 className="text-2xl text-center font-bold mb-4">Login</h2>
+        <h3 className="text-center mb-5"> If you don't have an account <a className='text-blue-700' href="./register/"> create one </a></h3>
+        <form>
+          <div>
+            <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+              Email
+            </label>
+            <input type="email" id="email"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-700 font-bold mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              placeholder="Enter your password"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-indigo-500 text-white font-bold py-2 px-4 rounded-md hover:bg-indigo-600"
+          >
+            Login
+          </button>
+        </form>
+        <p className="text-center text-blue-700 mt-3"><a>Forget your password?</a></p>
       </div>
+
 
     </div>
   );
--- app/(authpages)/register/page.tsx
+++ app/(authpages)/register/page.tsx
@@ -1,11 +1,23 @@
 const RegisterPage = () => {
   return (
-    <div className="flex items-center m-10 h-screen">
-      <div className="hidden md:flex md:w-2/3">  Login to your account </div>
-      <div className="w-1/3 bg-white rounded-lg shadow-lg p-8">
+    <div className="flex items-center h-screen">
+
+      <div className="hidden bg-slate-300 h-screen md:flex md:w-2/3  items-center">
+        <h2 className="m-auto text-2xl px-11 text-center font-extrabold"> Embark on an educational journey where learning meets thrilling adventure</h2>
+      </div>
+      <div className=" bg-white rounded-lg m-auto shadow-lg p-12">
         <h2 className="text-2xl text-center font-bold mb-4">Register</h2>
-        <h3 className="text-center"> If you have an account <a className='text-blue-700' href="./login/"> sign in </a></h3>
+        <h3 className="text-center mb-6"> If you already have an account <a className='text-blue-700' href="./login/"> sign in </a></h3>
         <form>
+          <div>
+            <label htmlFor="username" className="block text-gray-700 font-bold mb-2">
+              Username
+            </label>
+            <input type="email" id="email"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              placeholder="Enter your email"
+            />
+          </div>
           <div>
             <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
               Email
@@ -30,10 +42,12 @@ const RegisterPage = () => {
             type="submit"
             className="w-full bg-indigo-500 text-white font-bold py-2 px-4 rounded-md hover:bg-indigo-600"
           >
-            Register
+            Login
           </button>
         </form>
       </div>
+
+
     </div>
   );
 };
